3401a7d1427066057ada4047c151f6eeabcd1fa2
alghi <mo.alghifari@gmail.com>
2019-02-24 03:56:38 +0700

    Changed Place models

--- SeeWaves/app/src/main/java/com/sah/seewaves/models/Place.java
+++ SeeWaves/app/src/main/java/com/sah/seewaves/models/Place.java
@@ -1,17 +1,36 @@
 package com.sah.seewaves.models;
+import android.location.Location;
+
 import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @IgnoreExtraProperties
 public class Place {
-    public String location;
+    public Location location;
     public String status;
+    public Double latitude;
+    public Double longitude;
+    public String name;
 
     public Place()  {
 
     }
 
-    public Place(String location, String status)    {
-        this.location = location;
+    public Place(String name, Double latitude, Double longitude, String status)    {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.status = status;
+    }
+
+    public Map<String, Object> toMap()  {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("latitude", latitude);
+        result.put("longitude", longitude);
+        result.put("status", status);
+        return result;
     }
 }
