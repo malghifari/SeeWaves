3606da7a795d7fc0478dda465552fc5f69774977
alghi <mo.alghifari@gmail.com>
2019-02-25 16:23:45 +0700

    Added danger zone on tsunamitrigger

--- SeeWaves/app/src/main/java/com/sah/seewaves/MainActivity.java
+++ SeeWaves/app/src/main/java/com/sah/seewaves/MainActivity.java
@@ -1,10 +1,16 @@
 package com.sah.seewaves;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements
     private String mPhotoUrl;
     private GoogleApiClient mGoogleApiClient;
     private static TsunamiTrigger mTsunamiTrigger = null;
+    private static Place myPlace;
 
     public static final String ANONYMOUS = "anonymous";
     private static final String TAG = "MainActivity";
@@ -73,6 +80,24 @@ public class MainActivity extends AppCompatActivity implements
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
 
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        myPlace = new Place();
+        myPlace.longitude = location.getLongitude();
+        myPlace.latitude = location.getLatitude();
+        Log.d(TAG, "myPlace (latitude, longitude): " + myPlace.latitude + ", " + myPlace.longitude);
+        getTsunamiTrigger().writePlaceWithUpdateChildren("Rancabuaya", -6.893346, 107.610039, "safe");
     }
 
     @Override
@@ -93,7 +118,6 @@ public class MainActivity extends AppCompatActivity implements
                 finish();
                 return true;
             case R.id.action_settings:
-                getTsunamiTrigger().writePlaceWithUpdateChildren("Rancabuaya", -6.893346, 107.610039, "safe");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -112,9 +136,30 @@ public class MainActivity extends AppCompatActivity implements
 
     public static TsunamiTrigger getTsunamiTrigger()  {
         if (mTsunamiTrigger == null)   {
-            mTsunamiTrigger = new TsunamiTrigger();
+            mTsunamiTrigger = new TsunamiTrigger(myPlace);
         }
         return mTsunamiTrigger;
     }
 
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            myPlace.longitude = location.getLongitude();
+            myPlace.latitude = location.getLatitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
